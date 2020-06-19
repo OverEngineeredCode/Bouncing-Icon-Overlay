fccd0076a2a3d285570a020cd46b7df20fa93d6f
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-06-19 23:35:10 +0100

    Add a script for changing the speed

---
+++ scripts/ChangeSpeed.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+# First, we need to change to the main directory
+os.chdir(os.path.realpath(os.path.dirname(__file__) + "/.."))
+
+print("Welcome to the Change Speed Assistant!")
+print("This tool will allow you to change the movement speed of the overlay\n\n")
+
+newSpd = input("Enter the new speed:")
+
+configFile = open("config.js", "r")
+configText = configFile.read()
+configText = configText.replace("const config = ", "", 1)
+
+configJSON = json.loads(configText)
+configFile.close()
+
+configJSON["speed"] = int(newSpd)
+
+configFileWrite = open("config.js", "w")
+configFileWrite.write("const config = " + json.dumps(configJSON) + ";")
+
+print("Done! Configuration has been written")
+print("You may need to refresh to overlay for changes to take effect")
